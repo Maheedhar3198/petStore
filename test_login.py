69ae427f16d5ac57384b6db3bccbbe44608635a3
Maheedhar3198 <maheedhar.doddi@atmecs.com>
2025-07-29 12:39:05 +0530

    TuesdayTask1

--- test_login.py
+++ test_login.py
@@ -1,4 +1,4 @@
-import pytest
+from TestData import env
 from pytest_bdd import scenarios, given, when, then
 from playwright.sync_api import sync_playwright
 
@@ -6,17 +6,21 @@ playwright = sync_playwright().start()
 browser = playwright.chromium.launch(headless=False)
 page = browser.new_page()
 
+base_url = env.BASE_URL
+username = env.USERNAME
+password = env.PASSWORD
+
 scenarios('userSignin.feature')
 @given('user is on the login page')
 def test_loginpage():
-    page.goto('https://petstore.octoperf.com/actions/Account.action?signonForm=', wait_until="load")
+    page.goto(f"{base_url}/actions/Account.action?signonForm=", wait_until="load")
     assert page.get_by_text('Please enter your username and password.').is_visible(),f"Invalid page"
     print('user is in loginpage')
 
 @given('user enters a valid username and password')
 def test_entercredentials():
-    page.fill("//input[@name='username']","3198")
-    page.fill("//input[@name='password']", "Atmecs1223")
+    page.fill("//input[@name='username']",username)
+    page.fill("//input[@name='password']",password)
     print('user entered credentials')
 
 @when('user clicks on the login button')
